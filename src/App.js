41ce70cee25ed464e43161652ef36b733ebcd170
hiMinou <minou.matoko.officel@gmail.com>
2021-11-27 04:38:06 +0100

    progression de le clone du site web de tesla

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-    {/*<Header/>*/}
-    <Home/>
+      <Home/>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,67 @@
 import React from 'react'
+import styled from "styled-components"
+import Section from './Section'
 
 function Home() {
     return (
-        <div>
-            <h2>Home</h2>
-        </div>
+        <Container>
+            <Section 
+                title="Model S"
+                description="Commander en ligne ou tester en concession"
+                backgroundImg="model-s.jpg"
+                leftBtnText= "Personnaliser votre Tesla"
+                rightBtnText= "Voir les configurations existantes"
+            />
+
+            <Section
+                title="Model Y"
+                description="Commander en ligne ou tester en concession"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Personnaliser votre Tesla"
+                rightBtnText="Voir les configurations existantes"
+            />
+
+            <Section
+                title="Model 3"
+                description="Commander en ligne ou tester en concession"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Personnaliser votre Tesla"
+                rightBtnText="Voir les configurations existantes"
+            />
+
+            <Section
+                title="Model X"
+                description="Commander en ligne ou tester en concession"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Personnaliser votre Tesla"
+                rightBtnText="Voir les configurations existantes"
+            />
+            
+            <Section
+                title="Les meilleurs panneaux solaire"
+                description="Retour sur investissement garantit"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Commander maintenant"
+                rightBtnText="En savoir plus"
+            />
+
+            <Section
+                title="Le meilleur systeme de toit solaire "
+                description="Pour une energie 100% verte et un maison autonome en energie"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="Commander maintenant"
+                rightBtnText="En savoir plus"
+            />
+
+            
+            
+
+        </Container>
     )
 }
 
 export default Home
+
+const Container = styled.div`
+    height: 100vh;
+`
